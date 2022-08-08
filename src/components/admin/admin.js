6a5e73c445defa6d57c2583c0e230329a0593103
Leandro Farias <leandrofars@gmail.com>
2022-08-08 00:52:42 -0300

    feat(admin): show products

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -3,9 +3,18 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import './admin.css'
+import rightArrow from '../../imgs/chevron-right.svg'
+import leftArrow from '../../imgs/chevron-left.svg'
+import close from '../../imgs/close.svg'
+import trash from "../../imgs/trash.svg"
+
 export default function Admin(){
 
     const [loggedIn, setLoggedIn] = useState(false) 
+    const [estoque, setEstoque] = useState([])
+    const [displayBig, setDisplayBig] = useState(false)
+    const [productBig, setProductBig] =useState(null)
+    const [actualImg, setActualImg] =useState(null)
     let navigate = useNavigate()
 
     useEffect(()=>{
@@ -21,6 +30,15 @@ export default function Admin(){
     .catch(err=>{
         console.log(err)
     })
+    let urlProdutos = "http://localhost:5000/";
+    axios.get(urlProdutos)
+    .then(res=>{
+      console.log(res)
+      setEstoque(res.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
     },[])
 
     const handleLogout = () => {
@@ -29,11 +47,57 @@ export default function Admin(){
     }
 
     return(loggedIn?<div className="admin-page">
-        <p>Hello World</p>
+    <div className="products">
+    <div className="containerProdutos">
+      {
+        estoque.map(product=>
+        <div className="produtos"  key={product._id}>
+        <img src={require(`../../${product.imagem}`)} alt="arrival" onClick={()=>{
+          setDisplayBig(!displayBig); 
+          setProductBig(product.imagens);
+          setActualImg(0)}
+          }/>
+          <div className="informações">
+            <p className="info">{product.nome}<br/>
+            {product.preço}</p>
+            <ul className='sizes'>
+              {product.PP !== 0 && 
+              <li className='sizeIcon' key={"PP"}>PP</li>}
+              {product.P !==0 &&
+              <li className='sizeIcon' key={"P"}>P</li>}
+              {product.M !==0 &&
+              <li className='sizeIcon' key={"M"}>M</li>}
+              {product.G !==0 &&
+              <li className='sizeIcon' key={"G"}>G</li>}
+              {product.GG !==0 &&
+              <li className='sizeIcon' key={"GG"}>GG</li>}
+            </ul>
+            <span className="addSacola"><img src={trash} alt="sacola" /*onClick={add(product)}*//></span>
+          </div>
+        </div>)
+      }
+      {displayBig &&
+      <div className="product-max-container">
+        <div className='container-max'>
+          <div className='close'>
+          <img src={close} alt='close' className='closeImg' onClick={()=>{setDisplayBig(!displayBig)}}/>
+          </div>
+          <div className='image'>
+            <div className='arrow'>
+              {actualImg!==0 && <img src={leftArrow} alt='left-arrow' className='arrow' onClick={()=>setActualImg(old=>{return old-1})}/>}
+            </div>
+            <img src={require(`../../${productBig[actualImg]}`)} alt="product maximized" className='prodImage'/>
+            <div className='arrow'>
+              {actualImg===productBig.length-1 ? null:<img src={rightArrow} alt='right-arrow' className='arrow' onClick={()=>setActualImg(old=>{return old+1})}/>}
+            </div>
+          </div>
+        </div>
+      </div>}
+    </div>
+  </div>
             <div className="logout-submit-button">
                 <button className="submit-logout" onClick={handleLogout}>Logout</button>
             </div>
         </div>:<p>Forbidden</p>
     )
-    
 }
